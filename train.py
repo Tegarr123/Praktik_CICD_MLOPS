a36bf2939d73ac12d6878abb465ce324b0ee146a
Tegarr123 <fadhino987@gmail.com>
2025-04-09 02:26:27 +0000

    Update with new results

--- train.py
+++ train.py
@@ -19,27 +19,24 @@ y = drug_df.Drug.values
 
 # Split Data
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y,
-    test_size=0.3,
-    random_state=125
+    X, y, test_size=0.3, random_state=125
 )
 
 # ML pipeline
-cat_col = [1,2,3]
-num_col = [0,4]
+cat_col = [1, 2, 3]
+num_col = [0, 4]
 transform = ColumnTransformer(
-[
-("encoder", OrdinalEncoder(), cat_col),
-("num_imputer", SimpleImputer(strategy="median"), num_col),
-("num_scaler", StandardScaler(), num_col),
-]
+    [
+        ("encoder", OrdinalEncoder(), cat_col),
+        ("num_imputer", SimpleImputer(strategy="median"), num_col),
+        ("num_scaler", StandardScaler(), num_col),
+    ]
 )
 pipe = Pipeline(
-steps=[
-("preprocessing", transform),
-("model", RandomForestClassifier(n_estimators=100,
-random_state=125)),
-]
+    steps=[
+        ("preprocessing", transform),
+        ("model", RandomForestClassifier(n_estimators=100, random_state=125)),
+    ]
 )
 pipe.fit(X_train, y_train)
 
@@ -47,8 +44,7 @@ pipe.fit(X_train, y_train)
 predictions = pipe.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
 f1 = f1_score(y_test, predictions, average="macro")
-print("Accuracy:", str(round(accuracy, 2) * 100) + "%", "F1:", round(f1,
-2))
+print("Accuracy:", str(round(accuracy, 2) * 100) + "%", "F1:", round(f1, 2))
 
 with open("Results/metrics.txt", "w") as outfile:
     outfile.write(f"\nAccuracy = {round(accuracy, 2)}, F1 Score = {round(f1, 2)}.")
